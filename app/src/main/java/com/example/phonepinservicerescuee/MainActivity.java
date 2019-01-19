d32eb1e9c79b3e6a58c16d91f3f9a8f41273cc1a
liaomaos <liaomaos@msu.edu>
2019-01-19 08:00:55 -0500

    Added Maps Capability still needs fixing

--- app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
+++ app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.phonepinservicerescuee;
 
 import android.app.Notification;
-import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,6 +10,7 @@ import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -54,6 +55,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        init();
+    }
+
+    public void init(){
+        Button  btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void sendOnChannel1(View v){
