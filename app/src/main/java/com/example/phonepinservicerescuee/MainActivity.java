9ad7dab991b2c836fb32167f3b0866ed84f9d322
liaomaos <liaomaos@msu.edu>
2019-01-19 17:25:21 -0500

    Wav file

--- app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
+++ app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer Alarm = MediaPlayer.create(this,R.raw.Whistle.wav);
+        final MediaPlayer Alarm = MediaPlayer.create(this, R.raw.Alarm);
     }
 
 
