f09921c0f941c530adf93147f3828d11f5c6aea4
suanthon <suanthon@users.noreply.github.com>
2019-01-19 22:36:59 -0500

    dont touch

--- app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
+++ app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         final MediaPlayer AlarmPlayer = MediaPlayer.create(this, R.raw.alarm);
         AlarmPlayer.setLooping(true);
