0288ddb786b179b1809ffcce177afdd4730c0d99
suanthon <suanthon@users.noreply.github.com>
2019-01-20 10:13:54 -0500

    New Button bc confusion

--- app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
+++ app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
