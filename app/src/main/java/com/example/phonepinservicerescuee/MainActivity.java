d6646480c2d5aa45a5d6f5ba2192a5d5123266cb
liaomaos <liaomaos@msu.edu>
2019-01-20 09:49:56 -0500

    Temp Final Product

--- app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
+++ app/src/main/java/com/example/phonepinservicerescuee/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.phonepinservicerescuee;
 
+import android.Manifest;
 import android.app.Notification;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +21,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.phonepinservicerescuee.AppChannel.CHANNEL_2_ID;
 
@@ -27,8 +39,15 @@ import static com.example.phonepinservicerescuee.AppChannel.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String LONG = "Long";
+    public static final String LAT = "Lat";
+    public static final String TAG1 = "Saving Location Data";
+    private FirebaseFirestore mCollect = FirebaseFirestore.getInstance();
+    private FusedLocationProviderClient client;
 
     public static final String SOUND = "sound";
+
+
     private TextView mTextMessage;
     private NotificationManagerCompat notificationManager;
     private EditText editTextTitle;
@@ -50,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         final MediaPlayer AlarmPlayer = MediaPlayer.create(this, R.raw.alarm);
         AlarmPlayer.setLooping(true);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
 
         ToggleButton Searching = (ToggleButton) this.findViewById(R.id.toggleButton);
         Searching.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -57,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     Log.d(SOUND, "Successful Toggle");
+                    getLocation();
                     AlarmPlayer.start();
 
                 }else{
@@ -67,6 +89,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void getLocation(){
+        MapsActivity ma = new MapsActivity();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null){
+                        setLocation(location.getLongitude(),location.getLatitude());
+                    }
+                }
+            });
+        }
+        else{
+            ma.requestLocationPermission();
+
+        }
+
+
+    }
+
+    public void setLocation(double lon, double lat){
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put(LONG, lon);
+        dataToSave.put(LAT, lat);
+        mCollect.collection("GeoData")
+                .add(dataToSave)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG1, "Document Saved Successful" + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG1, "Error Adding Document" + e.toString());
+                    }
+                });
     }
 
 
